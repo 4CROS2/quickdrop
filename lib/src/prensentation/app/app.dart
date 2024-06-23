850c1992f63bde01277f31e1df978e10859267fa
yeferson rivas <yeferok2@gmail.com>
2024-06-23 04:42:54 -0500

    finished connection

---
+++ lib/src/prensentation/app/app.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:quickdrop/src/prensentation/app/cubit/app_cubit.dart';
+import 'package:quickdrop/src/prensentation/login/login.dart';
+
+class App extends StatefulWidget {
+  const App({super.key});
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider<AppCubit>(
+      create: (context) => AppCubit(),
+      child: MaterialApp(
+        home: Login(),
+      ),
+    );
+  }
+}
---
+++ lib/src/prensentation/app/cubit/app_state.dart
@@ -0,0 +1,8 @@
+part of 'app_cubit.dart';
+
+class AppState extends Equatable {
+  @override
+
+  List<Object?> get props => throw UnimplementedError();
+  
+}
