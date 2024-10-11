58a03a19ba0fceed1f480e1669db4449318e1efd
yeferson rivas <yeferok2@gmail.com>
2024-10-11 16:59:56 -0500

    update cubit

--- lib/src/prensentation/favorites/cubit/favorites_state.dart
+++ lib/src/prensentation/favorites/cubit/favorites_state.dart
@@ -4,7 +4,7 @@ sealed class FavoritesState extends Equatable {
   const FavoritesState();
 
   @override
-  List<Object> get props => [];
+  List<Object> get props => <Object>[];
 }
 
 final class FavoritesInitial extends FavoritesState {}
