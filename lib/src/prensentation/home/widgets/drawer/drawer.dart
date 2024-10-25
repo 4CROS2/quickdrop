59e22482b61808e18ec0fc2a6316e6a200618cb6
yeferson rivas <yeferok2@gmail.com>
2024-10-25 15:17:39 -0500

    drawer created

--- lib/src/prensentation/home/widgets/drawer/drawer.dart
+++ lib/src/prensentation/home/widgets/drawer/drawer.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:quickdrop/src/core/functions/page_navigation.dart';
 import 'package:quickdrop/src/domain/usecase/auth_usecase.dart';
 import 'package:quickdrop/src/injection/injection_container.dart';
 import 'package:quickdrop/src/prensentation/favorites/favorites.dart';
 import 'package:quickdrop/src/prensentation/home/widgets/drawer/widgets/custom_drawer_header/custom_drawer_header.dart';
+import 'package:quickdrop/src/prensentation/home/widgets/drawer/widgets/section/section_tile.dart';
 
 class HomeDrawer extends StatefulWidget {
   const HomeDrawer({super.key});
@@ -31,22 +31,26 @@ class _HomeDrawerState extends State<HomeDrawer> {
           mainAxisSize: MainAxisSize.max,
           children: <Widget>[
             CustomDrawerHeader(),
+            SectionTile(
+              page: Favorites(),
+              icon: Icons.favorite_outline_rounded,
+              label: 'mis favoritos',
+            ),
+            SectionTile(
+              icon: Icons.shopping_bag_outlined,
+              label: 'mis compras',
+            ),
+            SectionTile(
+              icon: Icons.schedule_rounded,
+              label: 'historial',
+            ),
             ElevatedButton(
               onPressed: () {
                 _authUseCase.logout();
               },
               child: const Text('exit'),
             ),
-            ElevatedButton(
-              onPressed: () {
-                PageNavigation.popNavigator(context);
-                PageNavigation.pushNavigator(
-                  context,
-                  page: Favorites(),
-                );
-              },
-              child: const Text('favorites'),
-            )
+            
           ],
         ),
       ),
