48df5baed4f752c7b96bd16207b7bcda30f831ab
yeferson rivas <yeferok2@gmail.com>
2024-11-12 14:35:20 -0500

    implemented

--- lib/src/presentation/product/widgets/productHeader/product_header.dart
+++ lib/src/presentation/product/widgets/productHeader/product_header.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import 'package:quickdrop/src/core/constants/constants.dart';
 import 'package:quickdrop/src/presentation/product/productCubit/product_cubit.dart';
@@ -11,10 +10,13 @@ import 'package:quickdrop/src/presentation/widgets/image_loader.dart';
 
 class ProductHeader extends SliverPersistentHeaderDelegate {
   ProductHeader({
+    required ProductDetailState state,
     required String previewImage,
-  }) : _previewImage = previewImage;
+  })  : _state = state,
+        _previewImage = previewImage;
 
   final String _previewImage;
+  final ProductDetailState _state;
 
   @override
   Widget build(
@@ -22,91 +24,87 @@ class ProductHeader extends SliverPersistentHeaderDelegate {
     final double percent = shrinkOffset / (maxExtent - minExtent);
     final double opacity =
         ((percent <= 0.4) ? 0.0 : (percent - 0.4) * 2).clamp(0.0, 1.0);
-    return BlocBuilder<ProductDetailCubit, ProductDetailState>(
-      builder: (BuildContext context, ProductDetailState state) {
-        return Material(
-          color: Colors.black12,
-          child: Stack(
-            fit: StackFit.expand,
-            children: <Widget>[
-              if (state is! SuccessLoadingProduct)
-                Hero(
-                  transitionOnUserGestures: true,
-                  tag: _previewImage,
-                  child: ClipperRadiusImages(
-                    child: ImageLoader(
-                      imageUrl: _previewImage,
-                    ),
-                  ),
+    return Material(
+      color: Colors.black12,
+      child: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          if (_state is SuccessLoadingProduct)
+            Carousel(
+              images: _state.product.baseImages,
+            ),
+          if (_state is! SuccessLoadingProduct)
+            Hero(
+              transitionOnUserGestures: true,
+              tag: _previewImage,
+              child: ClipperRadiusImages(
+                child: ImageLoader(
+                  imageUrl: _previewImage,
                 ),
-              if (state is SuccessLoadingProduct)
-                Carousel(
-                  images: state.product.baseImages,
-                ),
-              Positioned(
-                top: 0,
-                left: 0,
-                right: 0,
-                child: Opacity(
-                  opacity: opacity,
-                  child: SizedBox(
-                    height: minExtent,
-                    child: ClipRRect(
-                      child: BackdropFilter(
-                        filter: Constants.iamgeFilterBlur,
-                        child: DecoratedBox(
-                          decoration: BoxDecoration(
-                            color: Constants.secondaryColor,
-                          ),
-                        ),
+              ),
+            ),
+          Positioned(
+            top: 0,
+            left: 0,
+            right: 0,
+            child: Opacity(
+              opacity: opacity,
+              child: SizedBox(
+                height: minExtent,
+                child: ClipRRect(
+                  child: BackdropFilter(
+                    filter: Constants.iamgeFilterBlur,
+                    child: DecoratedBox(
+                      decoration: BoxDecoration(
+                        color: Constants.secondaryColor,
                       ),
                     ),
                   ),
                 ),
               ),
-              Positioned(
-                top: 0,
-                left: 0,
-                right: 0,
-                child: SafeArea(
-                  child: Padding(
-                    padding: EdgeInsets.symmetric(
-                      horizontal: Constants.mainPadding.right,
-                    ).copyWith(top: 8),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            ),
+          ),
+          Positioned(
+            top: 0,
+            left: 0,
+            right: 0,
+            child: SafeArea(
+              child: Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: Constants.mainPadding.right,
+                ).copyWith(top: 8),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Row(
+                      mainAxisSize: MainAxisSize.min,
                       children: <Widget>[
-                        Row(
-                          mainAxisSize: MainAxisSize.min,
-                          children: <Widget>[
-                            HeaderButtons(
-                              onTap: () => context.go('/home'),
-                              icon: Icons.arrow_back_ios_new_rounded,
-                              backgroundColor: Colors.black45,
-                              iconColor: Colors.white,
-                            ),
-                            TitleHeader(
-                              title: state is SuccessLoadingProduct
-                                  ? state.product.name
-                                  : '',
-                              showText: percent >= 1.1,
-                            ),
-                          ],
-                        ),
-                        const HeaderButtons(
-                          icon: Icons.shopping_cart_outlined,
+                        HeaderButtons(
+                          onTap: () => context.go('/home'),
+                          icon: Icons.arrow_back_ios_new_rounded,
                           backgroundColor: Colors.black45,
                           iconColor: Colors.white,
-                        )
+                        ),
+                        TitleHeader(
+                          title: _state is SuccessLoadingProduct
+                              ? (_state).product.name
+                              : '',
+                          showText: percent >= 1.1,
+                        ),
                       ],
                     ),
-                  ),
+                    const HeaderButtons(
+                      icon: Icons.shopping_cart_outlined,
+                      backgroundColor: Colors.black45,
+                      iconColor: Colors.white,
+                    )
+                  ],
                 ),
               ),
-            ],
+            ),
           ),
-        );
-      },
+        ],
+      ),
     );
   }
 
