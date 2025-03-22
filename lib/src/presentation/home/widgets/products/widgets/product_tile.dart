67402dcf064e95a98367c0cbb4c0137eeada7658
yeferson rivas <yeferok2@gmail.com>
2025-03-22 13:56:01 -0500

    Refactor ProductTile to StatefulWidget and integrate custom rating bar for product ratings

--- lib/src/presentation/home/widgets/products/widgets/product_tile.dart
+++ lib/src/presentation/home/widgets/products/widgets/product_tile.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+
+import 'package:custom_rating_bar/custom_rating_bar.dart';
 import 'package:extensions/extensions.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
@@ -8,7 +11,7 @@ import 'package:quickdrop/src/presentation/home/widgets/products/widgets/favorit
 import 'package:quickdrop/src/presentation/widgets/clipper_radius_images.dart';
 import 'package:quickdrop/src/presentation/widgets/image_loader.dart';
 
-class ProductTile extends StatelessWidget {
+class ProductTile extends StatefulWidget {
   const ProductTile({
     required ProductsEntity product,
     super.key,
@@ -16,20 +19,59 @@ class ProductTile extends StatelessWidget {
   final ProductsEntity _product;
 
   @override
+  State<ProductTile> createState() => _ProductTileState();
+}
+
+class _ProductTileState extends State<ProductTile> {
+  late final Size size;
+
+  @override
+  void initState() {
+    super.initState();
+    _getImageSize(
+      imageUrl: widget._product.baseImages.first,
+    );
+  }
+
+  Future<void> _getImageSize({required String imageUrl}) async {
+    final Image image = Image.network(imageUrl);
+    final ImageStream imageStream =
+        image.image.resolve(const ImageConfiguration());
+    final Completer<void> completer = Completer<void>();
+
+    imageStream.addListener(
+      ImageStreamListener((ImageInfo info, bool _) {
+        final Size size = Size(
+          info.image.width.toDouble(),
+          info.image.height.toDouble(),
+        );
+        setState(() {
+          this.size = size;
+        });
+        completer.complete();
+      }),
+    );
+
+    await completer.future;
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Padding(
       padding: Constants.mainPadding / 2,
       child: Material(
-        elevation: 2,
+        elevation: 12,
         clipBehavior: Clip.hardEdge,
         borderRadius: Constants.mainBorderRadius,
+        shadowColor: Colors.transparent,
+        surfaceTintColor: Constants.primaryColor,
         child: InkWell(
           onTap: () {
             context.push(
               Uri(
-                path: '/product/${_product.id}',
+                path: '/product/${widget._product.id}',
                 queryParameters: <String, dynamic>{
-                  'previewImage': _product.baseImages.first
+                  'previewImage': widget._product.baseImages.first
                 },
               ).toString(),
             );
@@ -44,11 +86,14 @@ class ProductTile extends StatelessWidget {
                   children: <Widget>[
                     Hero(
                       transitionOnUserGestures: true,
-                      tag: _product.baseImages.first,
+                      tag: widget._product.baseImages.first,
                       child: ClipperRadiusImages(
                         borderRadius: Constants.mainBorderRadius / 2,
-                        child: ImageLoader(
-                          imageUrl: _product.baseImages[0],
+                        child: SizedBox(
+                          height: size.height * .2,
+                          child: ImageLoader(
+                            imageUrl: widget._product.baseImages[0],
+                          ),
                         ),
                       ),
                     ),
@@ -56,7 +101,7 @@ class ProductTile extends StatelessWidget {
                       top: Constants.buttonPadding.top / 2,
                       right: Constants.buttonPadding.right / 2,
                       child: AddToFavoriteButton(
-                        productId: _product.id,
+                        productId: widget._product.id,
                       ),
                     )
                   ],
@@ -65,22 +110,44 @@ class ProductTile extends StatelessWidget {
                 Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: <Widget>[
+                    //product rating
+                    Row(
+                      spacing: Constants.mainPaddingValue / 2,
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: <Widget>[
+                        RatingBar.readOnly(
+                          filledIcon: Icons.star,
+                          emptyIcon: Icons.star_border,
+                          initialRating: 4,
+                          maxRating: 5,
+                          size: 18,
+                        ),
+                        Text(
+                          '(20)',
+                          style: const TextStyle(
+                            fontWeight: FontWeight.w900,
+                            fontSize: 12,
+                            color: Colors.grey,
+                          ),
+                        )
+                      ],
+                    ),
                     //product name
                     Text(
-                      _product.name.capitalize(),
+                      widget._product.name.capitalize(),
                       softWrap: true,
                       style: const TextStyle(
-                        fontWeight: FontWeight.w600,
-                        fontSize: 14,
+                        fontWeight: FontWeight.w900,
+                        fontSize: 18,
                       ),
                     ),
                     //product price
                     Text(
-                      formatPrice(_product.basePrice),
+                      formatPrice(widget._product.basePrice),
                       style: const TextStyle(
-                        fontWeight: FontWeight.w900,
-                        fontSize: 16,
-                      ),
+                          fontWeight: FontWeight.w900,
+                          fontSize: 16,
+                          color: Constants.primaryColor),
                     )
                   ],
                 ),
