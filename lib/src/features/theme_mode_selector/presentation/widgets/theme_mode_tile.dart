238798ea0e3aaf5a7f6dc379cba6a9214f48aa0e
yeferson rivas <yeferok2@gmail.com>
2025-04-16 23:12:24 -0500

    fix(theme_mode_tile): update theme state management for improved performance

--- lib/src/features/theme_mode_selector/presentation/widgets/theme_mode_tile.dart
+++ lib/src/features/theme_mode_selector/presentation/widgets/theme_mode_tile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:quickdrop/src/core/constants/constants.dart';
 import 'package:quickdrop/src/features/app/cubit/app_cubit.dart';
 import 'package:quickdrop/src/injection/injection_barrel.dart';
@@ -31,50 +32,57 @@ class _ThemeModeTileState extends State<ThemeModeTile> {
 
   @override
   Widget build(BuildContext context) {
-    final bool isActive = _appCubit.state.themeMode == widget.themeMode;
-    return Padding(
-      padding: Constants.mainPadding / 2,
-      child: GestureDetector(
-        onTap: _changeTheme,
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            AnimatedSlide(
-              duration: const Duration(milliseconds: 300),
-              offset: isActive ? const Offset(0, -0.1) : Offset.zero,
-              child: Icon(widget.icon),
+    return BlocSelector<AppCubit, AppState, bool>(
+      selector: (AppState state) {
+        return state.themeMode == widget.themeMode;
+      },
+      builder: (BuildContext context, bool state) {
+        return Padding(
+          padding: Constants.mainPadding / 2,
+          child: GestureDetector(
+            onTap: _changeTheme,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                AnimatedSlide(
+                  duration: const Duration(milliseconds: 300),
+                  offset: state ? const Offset(0, -0.1) : Offset.zero,
+                  child: Icon(widget.icon),
+                ),
+                AnimatedSwitcher(
+                  duration: Constants.animationTransition,
+                  switchInCurve: Curves.easeIn,
+                  switchOutCurve: Curves.easeOut,
+                  transitionBuilder:
+                      (Widget child, Animation<double> animation) {
+                    return FadeTransition(
+                      opacity: animation,
+                      child: child,
+                    );
+                  },
+                  child: state
+                      ? DecoratedBox(
+                          key: const ValueKey<String>('active'),
+                          decoration: BoxDecoration(
+                            color: Theme.of(context).textTheme.bodyLarge!.color,
+                            borderRadius: Constants.mainBorderRadius,
+                          ),
+                          child: const SizedBox(
+                            width: 10,
+                            height: 2,
+                          ),
+                        )
+                      : const SizedBox(
+                          key: ValueKey<String>('inactive'),
+                          width: 10,
+                          height: 2,
+                        ),
+                ),
+              ],
             ),
-            AnimatedSwitcher(
-              duration: Constants.animationTransition,
-              switchInCurve: Curves.easeIn,
-              switchOutCurve: Curves.easeOut,
-              transitionBuilder: (Widget child, Animation<double> animation) {
-                return FadeTransition(
-                  opacity: animation,
-                  child: child,
-                );
-              },
-              child: isActive
-                  ? DecoratedBox(
-                      key: const ValueKey<String>('active'),
-                      decoration: BoxDecoration(
-                        color: Theme.of(context).textTheme.bodyLarge!.color,
-                        borderRadius: Constants.mainBorderRadius,
-                      ),
-                      child: const SizedBox(
-                        width: 10,
-                        height: 2,
-                      ),
-                    )
-                  : const SizedBox(
-                      key: ValueKey<String>('inactive'),
-                      width: 10,
-                      height: 2,
-                    ),
-            ),
-          ],
-        ),
-      ),
+          ),
+        );
+      },
     );
   }
 }
